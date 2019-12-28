cf10d51d34123238608723ec572162cfa818e081
walonsor <walteralonso20@yahoo.com>
2019-12-28 10:24:30 -0500

    iframe

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  url: any;
 
+  constructor(public navCtrl: NavController, private sanitize: DomSanitizer) {
+    
+  }
+
+  urlpaste(){
+    this.url = "https://gps.widetech.co/app/app.html?s=NCxqYWxmb25zbzE%3D";
+    return this.sanitize.bypassSecurityTrustResourceUrl(this.url);
   }
 
 }
